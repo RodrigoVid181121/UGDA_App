5b34bb1e2d34ee4946c7d818f7a4034f4c44d948
EsmeRod <89407510+EsmeRod@users.noreply.github.com>
2023-05-10 11:17:07 -0600

    Update SP_Usuarios.sql

--- Archivos SQL/SP_Usuarios.sql
+++ Archivos SQL/SP_Usuarios.sql
@@ -7,13 +7,13 @@ go
 
 create procedure SP_UpdateUsu
 
-@nombre varchar(50), @apellido varchar(50), @carnet nchar(6), @correo varchar(150), @contrasenia varchar(20), @idcargo int
+@nombre varchar(50), @apellido varchar(50), @carnet nchar(6), @correo varchar(150), @contrasenia varchar(20)
 as
 DECLARE @Idus int
 if EXISTS(SELECT id_usuario from usuarios where carnet = @carnet) 
 	SET @Idus = (SELECT id_usuario from usuarios where carnet = @carnet)
 	 
-UPDATE usuarios set nombre=@nombre, apellido=@apellido, carnet=@carnet, correo=@correo, contrasenia=@contrasenia, id_cargo=@idcargo
+UPDATE usuarios set nombre=@nombre, apellido=@apellido, carnet=@carnet, correo=@correo, contrasenia=@contrasenia
 WHERE id_usuario=@Idus
 go
 
@@ -32,7 +32,7 @@ go
 Create procedure SP_Usu_Index
 as
 begin
-SELECT u.carnet as Carnet, u.nombre as Nombre, u.apellido as Apellido, u.correo as Correo, u.contrasenia as Contrasenia, c.nombre_cargo as Cargo
+SELECT u.carnet as Carnet, u.nombre as Nombre, u.apellido as Apellido, u.correo as Correo, c.nombre_cargo as Cargo
  FROM usuarios u 
  Inner JOIN cargos c ON c.id_cargo = u.id_cargo
  end 
